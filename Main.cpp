5dd54dc0ea2f86b236af5ed8f45c72209206618d
Sohailaaa <98600518+Sohailaaa@users.noreply.github.com>
2023-11-29 00:00:58 +0200

    Added third person and first person movement

--- Main.cpp
+++ Main.cpp
@@ -6,51 +6,76 @@
 #include "Model_3DS.h"
 #include "GLTexture.h"
 #include <glut.h>
+#include <iostream>
+#include <math.h>
 
 //=======================================================================
 // Constants
 //=======================================================================
 
+#define DEG2RAD(a) (a * 0.0174532925)
+
 int WIDTH = 1280;
 int HEIGHT = 720;
+
+char title[] = "Aladdin Game";
 
 // 3D Projection Options
 GLdouble fovy = 45.0;
 GLdouble aspectRatio = (GLdouble)WIDTH / (GLdouble)HEIGHT;
 GLdouble zNear = 0.1;
-GLdouble zFar = 100;
+GLdouble zFar = 100000;
+
 
 
 //=======================================================================
 // Classes
 //=======================================================================
 
-class Vector
-{
+class Vector {
 public:
-	GLdouble x, y, z;
-	Vector() {}
-	Vector(GLdouble _x, GLdouble _y, GLdouble _z) : x(_x), y(_y), z(_z) {}
-	//================================================================================================//
-	// Operator Overloading; In C++ you can override the behavior of operators for you class objects. //
-	// Here we are overloading the += operator to add a given value to all vector coordinates.        //
-	//================================================================================================//
-	void operator +=(float value)
-	{
-		x += value;
-		y += value;
-		z += value;
+	float x, y, z;
+
+	Vector(float _x = 0.0f, float _y = 0.0f, float _z = 0.0f) {
+		x = _x;
+		y = _y;
+		z = _z;
+	}
+
+	Vector operator+(Vector& v) {
+		return Vector(x + v.x, y + v.y, z + v.z);
+	}
+
+	Vector operator-(Vector& v) {
+		return Vector(x - v.x, y - v.y, z - v.z);
+	}
+	Vector operator*(float n) {
+		return Vector(x * n, y * n, z * n);
+	}
+
+	Vector operator/(float n) {
+		return Vector(x / n, y / n, z / n);
+	}
+
+	Vector unit() {
+		return *this / sqrt(x * x + y * y + z * z);
+	}
+
+	Vector cross(Vector v) {
+		return Vector(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
 	}
 };
 
 class GameObject {
+
+
+public:
 	Vector position;
 	float rotation;
 	float scale;
 	float collisionRadius;
 	Model_3DS gameObjectModel;
 
-public:
 
 	GameObject() {
 	}
@@ -89,34 +114,101 @@ public:
 		glPopMatrix();
 	}
 
-
 };
+
+class Camera {
+public:
+	Vector eye, center, up;
+
+
+	Camera(float eyeX = 3.6f, float eyeY = 0.0f, float eyeZ = 2.0f, float centerX = 0.0f, float centerY = 0.0f, float centerZ = 2.0f, float upX = 0.0f, float upY = 1.0f, float upZ = 0.0f) {
+		eye = Vector(eyeX, eyeY, eyeZ);
+		center = Vector(centerX, centerY, centerZ);
+		up = Vector(upX, upY, upZ);
+	}
+
+	void moveX(float d) {
+		Vector right = up.cross(center - eye).unit();
+		eye = eye + right * d;
+		center = center + right * d;
+	}
+
+	void moveY(float d) {
+		eye = eye + up.unit() * d;
+		center = center + up.unit() * d;
+	}
+
+	void moveZ(float d) {
+		Vector view = (center - eye).unit();
+		eye = eye + view * d;
+		center = center + view * d;
+	}
+
+	void rotateX(float a) {
+		Vector view = (center - eye).unit();
+		Vector right = up.cross(view).unit();
+		view = view * cos(DEG2RAD(a)) + up * sin(DEG2RAD(a));
+		up = view.cross(right);
+		center = eye + view;
+	}
+
+	void rotateY(float a) {
+		Vector view = (center - eye).unit();
+		Vector right = up.cross(view).unit();
+		view = view * cos(DEG2RAD(a)) + right * sin(DEG2RAD(a));
+		right = view.cross(up);
+		center = eye + view;
+	}
+
+	void look() {
+		gluLookAt(
+			eye.x, eye.y, eye.z,
+			center.x, center.y, center.z,
+			up.x, up.y, up.z
+		);
+	}
+};
+
+
+
 //=======================================================================
 // Variables
 //=======================================================================
 
-char title[] = "Aladdin Game";
-
 GLuint tex;
 
-Vector Eye(20, 5, 20);
-Vector At(0, 0, 0);
-Vector Up(0, 1, 0);
-
 int cameraZoom = 0;
+
+float cameraDistanceFromPlayer = 17.0f;
+float cameraHeightAbovePlayer = 10.0f;
 
 // Game Objects
 GameObject aladdin;
 GameObject snake;
+GameObject bottle;
 
 // Textures
 GLTexture tex_ground;
 
+// State
+bool firstPersonModeOn = false;
+int movementState = 0;
 
 
+Camera camera;
 //=======================================================================
-// Model Loading Function
+// Set Up Camera Function
 //=======================================================================
+
+void setupCamera() {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(fovy, aspectRatio, zNear, zFar);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	camera.look();
+}
 
 //=======================================================================
 // Lighting Configuration Function
@@ -175,22 +267,9 @@ void myInit(void)
 {
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 
-	glMatrixMode(GL_PROJECTION);
 
-	glLoadIdentity();
 
-	gluPerspective(fovy, aspectRatio, zNear, zFar);
-	//*******************************************************************************************//
-	// fovy:			Angle between the bottom and top of the projectors, in degrees.			 //
-	// aspectRatio:		Ratio of width to height of the clipping plane.							 //
-	// zNear and zFar:	Specify the front and back clipping planes distances from camera.		 //
-	//*******************************************************************************************//
-
-	glMatrixMode(GL_MODELVIEW);
-
-	glLoadIdentity();
-
-	gluLookAt(Eye.x, Eye.y, Eye.z, At.x, At.y, At.z, Up.x, Up.y, Up.z);
+	setupCamera();
 	//*******************************************************************************************//
 	// EYE (ex, ey, ez): defines the location of the camera.									 //
 	// AT (ax, ay, az):	 denotes the direction where the camera is aiming at.					 //
@@ -198,11 +277,14 @@ void myInit(void)
 	//*******************************************************************************************//
 
 	InitLightSource();
-
 	InitMaterial();
 
-	glEnable(GL_DEPTH_TEST);
+	// {PositionX, PositionY, PositionZ }, Rotation, Scale, Collision Radius, "Path to model file"
+	aladdin = GameObject({ 0,0,0 }, 0, 0.04, 0.5, "models/aladdin/aladdin.3ds");
+	snake = GameObject({ 7,0,0.9 }, 0, 0.03, 0.5, "models/snake/snake.3ds");
+	bottle = GameObject({ -7,2,0.9 }, 0, 0.08, 0.5, "models/bottle/bottle.3ds");
 
+	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_NORMALIZE);
 }
 
@@ -220,17 +302,18 @@ void RenderGround()
 	glBindTexture(GL_TEXTURE_2D, tex_ground.texture[0]);	// Bind the ground texture
 
 	glPushMatrix();
-	glBegin(GL_QUADS);
-	glNormal3f(0, 1, 0);	// Set quad normal direction.
-	glTexCoord2f(0, 0);		// Set tex coordinates ( Using (0,0) -> (5,5) with texture wrapping set to GL_REPEAT to simulate the ground repeated grass texture).
-	glVertex3f(-20, 0, -20);
-	glTexCoord2f(5, 0);
-	glVertex3f(20, 0, -20);
-	glTexCoord2f(5, 5);
-	glVertex3f(20, 0, 20);
-	glTexCoord2f(0, 5);
-	glVertex3f(-20, 0, 20);
-	glEnd();
+		glScalef(3.0, 3.0, 3.0);	// Scale the ground quad	
+		glBegin(GL_QUADS);
+			glNormal3f(0, 1, 0);	// Set quad normal direction.
+			glTexCoord2f(0, 0);		// Set tex coordinates ( Using (0,0) -> (5,5) with texture wrapping set to GL_REPEAT to simulate the ground repeated grass texture).
+			glVertex3f(-20, 0, -20);
+			glTexCoord2f(5, 0);
+			glVertex3f(20, 0, -20);
+			glTexCoord2f(5, 5);
+			glVertex3f(20, 0, 20);
+			glTexCoord2f(0, 5);
+			glVertex3f(-20, 0, 20);
+		glEnd();
 	glPopMatrix();
 
 	glEnable(GL_LIGHTING);	// Enable lighting again for other entites coming throung the pipeline.
@@ -245,8 +328,6 @@ void myDisplay(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-
-
 	GLfloat lightIntensity[] = { 0.7, 0.7, 0.7, 1.0f };
 	GLfloat lightPosition[] = { 0.0f, 100.0f, 0.0f, 0.0f };
 	glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
@@ -255,46 +336,10 @@ void myDisplay(void)
 	// Draw Ground
 	RenderGround();
 
-	// Draw Tree Model
-	//glPushMatrix();
-	//glTranslatef(10, 0, 0);
-	//glScalef(0.7, 0.7, 0.7);
-	//model_tree.Draw();
-	//glPopMatrix();
-
-	//// Draw house Model
-	//glPushMatrix();
-	//glRotatef(90.f, 1, 0, 0);
-	//model_house.Draw();
-	//glPopMatrix();
-
-	// Draw house Model
-	glPushMatrix();
-	glScalef(0.1, 0.1, 0.1);
-
-	//model_home.Draw();
-	glPopMatrix();
-
-	//glPushMatrix();
-	//glScalef(0.2, 0.2, 0.2);
-	//model_water.Draw();
-	//glPopMatrix();
-
-	//glPushMatrix();
-	//glScalef(0.2, 0.2, 0.2);
-	//model_snake.Draw();
-	//glPopMatrix();
-
-	//glPushMatrix();
-	//glScalef(0.2, 0.2, 0.2);
-	//model_rock.Draw();
-	//glPopMatrix();
-
-
+	// Drawing the Game Objects
 	aladdin.draw();
-
-
-
+	snake.draw();
+	bottle.draw();
 
 	//sky box
 	glPushMatrix();
@@ -313,6 +358,63 @@ void myDisplay(void)
 }
 
 //=======================================================================
+// Camera Position Function
+//=======================================================================
+
+void setCameraFollow() {
+	
+	float playerRotationAngle = aladdin.rotation;
+
+
+
+	//float cameraPositionX = 0;
+	//float cameraPositionZ = 0;
+
+	// Set Camera Position Relative to player
+	float cameraPositionX = -std::sinf(DEG2RAD(playerRotationAngle)) * cameraDistanceFromPlayer;
+	float cameraPositionZ = -std::cosf(DEG2RAD(playerRotationAngle)) * cameraDistanceFromPlayer;
+
+	cameraPositionX += aladdin.position.x;
+	cameraPositionZ += aladdin.position.z;
+
+	float cameraPositionY = aladdin.position.y + cameraHeightAbovePlayer;
+	camera.eye = { cameraPositionX,cameraPositionY,cameraPositionZ };
+
+	// Set Camera Target
+	Vector cameraCenter = aladdin.position;
+		
+
+	if (firstPersonModeOn) {
+		float cameraTargetX = std::sinf(DEG2RAD(playerRotationAngle)) * cameraDistanceFromPlayer*10;
+		float cameraTargetZ = std::cosf(DEG2RAD(playerRotationAngle)) * cameraDistanceFromPlayer*10;
+
+		cameraCenter.x -= cameraTargetX;
+		cameraCenter.z -= cameraTargetZ;
+	}
+
+	cameraCenter.y += 7;
+	camera.center = cameraCenter;
+	
+}
+
+//=======================================================================
+// Timer Function
+//=======================================================================
+
+void myTimer(int) {
+
+	
+	setCameraFollow();
+	setupCamera();
+
+
+
+	glutPostRedisplay();
+	glutTimerFunc(1000 / 60, myTimer, 0);
+
+}
+
+//=======================================================================
 // Keyboard Function
 //=======================================================================
 void myKeyboard(unsigned char button, int x, int y)
@@ -321,9 +423,22 @@ void myKeyboard(unsigned char button, int x, int y)
 	{
 	case 'w':
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		
 		break;
 	case 'r':
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		break;
+	case 'f':
+		if (!firstPersonModeOn) {
+			firstPersonModeOn = true;
+			cameraDistanceFromPlayer = -3;
+			cameraHeightAbovePlayer = 7;
+		}
+		else {
+			firstPersonModeOn = false;
+			cameraDistanceFromPlayer = 17.0f;
+			cameraHeightAbovePlayer = 10.0f;
+		}
 		break;
 	case 27:
 		exit(0);
@@ -332,7 +447,36 @@ void myKeyboard(unsigned char button, int x, int y)
 		break;
 	}
 
-	glutPostRedisplay();
+}
+
+//=======================================================================
+// Special Function
+//=======================================================================
+void mySpecial(int key, int x, int y) {
+	float a = 1.0;
+
+	const float xChange[] = {0,-1,0,1};
+	const float zChange[] = {1,0,-1,0};
+
+	switch (key) {
+	case GLUT_KEY_UP:
+		aladdin.position.x += xChange[movementState];
+		aladdin.position.z += zChange[movementState];
+		break;
+	case GLUT_KEY_DOWN:
+		movementState = (movementState + 2) % 4;
+		aladdin.setRotation(aladdin.rotation - 180);
+		break;
+	case GLUT_KEY_LEFT:
+		movementState = (movementState - 1 + 4) % 4;
+		aladdin.setRotation(aladdin.rotation + 90);
+		break;
+	case GLUT_KEY_RIGHT:
+		movementState = (movementState + 1) % 4;
+		aladdin.setRotation(aladdin.rotation - 90);
+		break;
+	}
+
 }
 
 //=======================================================================
@@ -340,29 +484,14 @@ void myKeyboard(unsigned char button, int x, int y)
 //=======================================================================
 void myMotion(int x, int y)
 {
-	y = HEIGHT - y;
 
-	if (cameraZoom - y > 0)
-	{
-		Eye.x += -0.1;
-		Eye.z += -0.1;
-	}
-	else
-	{
-		Eye.x += 0.1;
-		Eye.z += 0.1;
-	}
 
-	cameraZoom = y;
-
-	glLoadIdentity();	//Clear Model_View Matrix
-
-	gluLookAt(Eye.x, Eye.y, Eye.z, At.x, At.y, At.z, Up.x, Up.y, Up.z);	//Setup Camera with modified paramters
+	setupCamera();
 
 	GLfloat light_position[] = { 0.0f, 10.0f, 0.0f, 1.0f };
 	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 
-	glutPostRedisplay();	//Re-draw scene 
+	
 }
 
 //=======================================================================
@@ -401,7 +530,7 @@ void myReshape(int w, int h)
 	// go back to modelview matrix so we can move the objects about
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt(Eye.x, Eye.y, Eye.z, At.x, At.y, At.z, Up.x, Up.y, Up.z);
+	setupCamera();
 }
 
 //=======================================================================
@@ -409,16 +538,8 @@ void myReshape(int w, int h)
 //=======================================================================
 void LoadAssets()
 {
-	// Loading Model filesman
-	aladdin = GameObject({ 0,0,0 }, 0, 0.1, 0.5, "models/aladdin/aladdin.3ds");
-	snake = GameObject({ 0,0,0 }, 0, 0.1, 0.5, "models/aladdin/aladdin.3ds");
-
-
-
-	// model_tree.Load("Models/tree/Tree1.3ds");
-
 	// Loading texture files
-	tex_ground.Load("Textures/ground.bmp");
+	tex_ground.Load("Textures/sand.bmp");
 	loadBMP(&tex, "Textures/blu-sky-3.bmp", true);
 }
 
@@ -433,22 +554,16 @@ void main(int argc, char** argv)
 	glutInit(&argc, argv);
 
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-
 	glutInitWindowSize(WIDTH, HEIGHT);
-
 	glutInitWindowPosition(100, 150);
-
 	glutCreateWindow(title);
-
 	glutDisplayFunc(myDisplay);
-
+	glutTimerFunc(0, myTimer, 0);
 	glutKeyboardFunc(myKeyboard);
-
-	glutMotionFunc(myMotion);
-
-	glutMouseFunc(myMouse);
-
-	glutReshapeFunc(myReshape);
+	glutSpecialFunc(mySpecial);
+	// glutMotionFunc(myMotion);
+	// glutMouseFunc(myMouse);
+	// glutReshapeFunc(myReshape);
 
 	myInit();
 
