edf572f8a51fb40b904508aec5d9958cc2b67b00
Sohailaaa <98600518+Sohailaaa@users.noreply.github.com>
2023-11-28 21:40:47 +0200

    Initial commit
    Added OpenGLMeshLoader from CMS and other assets.

---
+++ Main.cpp
@@ -0,0 +1,466 @@
+//=======================================================================
+// Includes
+//=======================================================================
+
+#include "TextureBuilder.h"
+#include "Model_3DS.h"
+#include "GLTexture.h"
+#include <glut.h>
+
+//=======================================================================
+// Constants
+//=======================================================================
+
+int WIDTH = 1280;
+int HEIGHT = 720;
+
+// 3D Projection Options
+GLdouble fovy = 45.0;
+GLdouble aspectRatio = (GLdouble)WIDTH / (GLdouble)HEIGHT;
+GLdouble zNear = 0.1;
+GLdouble zFar = 100;
+
+
+//=======================================================================
+// Classes
+//=======================================================================
+
+class Vector
+{
+public:
+	GLdouble x, y, z;
+	Vector() {}
+	Vector(GLdouble _x, GLdouble _y, GLdouble _z) : x(_x), y(_y), z(_z) {}
+	//================================================================================================//
+	// Operator Overloading; In C++ you can override the behavior of operators for you class objects. //
+	// Here we are overloading the += operator to add a given value to all vector coordinates.        //
+	//================================================================================================//
+	void operator +=(float value)
+	{
+		x += value;
+		y += value;
+		z += value;
+	}
+};
+
+class GameObject {
+	Vector position;
+	float rotation;
+	float scale;
+	float collisionRadius;
+	Model_3DS gameObjectModel;
+
+public:
+
+	GameObject() {
+	}
+
+
+	GameObject(Vector position, float rotation, float scale, float collisionRadius, char* pathToModel) {
+		this->position = position;
+		this->rotation = rotation;
+		this->scale = scale;
+		this->collisionRadius = collisionRadius;
+		gameObjectModel.Load(pathToModel);
+	}
+
+	void setPosition(Vector position) {
+		this->position = position;
+	}
+
+	void setRotation(float rotation) {
+		this->rotation = rotation;
+	}
+
+	void setScale(float scale) {
+		this->scale = scale;
+	}
+
+	void setCollisionRadius(float collisionRadius) {
+		this->collisionRadius = collisionRadius;
+	}
+
+	void draw() {
+		glPushMatrix();
+		glTranslatef(position.x, position.y, position.z);
+		glRotatef(rotation, 0, 1, 0);
+		glScalef(scale, scale, scale);
+		gameObjectModel.Draw();
+		glPopMatrix();
+	}
+
+
+};
+//=======================================================================
+// Variables
+//=======================================================================
+
+char title[] = "Aladdin Game";
+
+GLuint tex;
+
+Vector Eye(20, 5, 20);
+Vector At(0, 0, 0);
+Vector Up(0, 1, 0);
+
+int cameraZoom = 0;
+
+// Game Objects
+GameObject aladdin;
+GameObject snake;
+
+// Textures
+GLTexture tex_ground;
+
+
+
+//=======================================================================
+// Model Loading Function
+//=======================================================================
+
+//=======================================================================
+// Lighting Configuration Function
+//=======================================================================
+void InitLightSource()
+{
+	// Enable Lighting for this OpenGL Program
+	glEnable(GL_LIGHTING);
+
+	// Enable Light Source number 0
+	// OpengL has 8 light sources
+	glEnable(GL_LIGHT0);
+
+	// Define Light source 0 ambient light
+	GLfloat ambient[] = { 0.1f, 0.1f, 0.1, 1.0f };
+	glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
+
+	// Define Light source 0 diffuse light
+	GLfloat diffuse[] = { 0.5f, 0.5f, 0.5f, 1.0f };
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
+
+	// Define Light source 0 Specular light
+	GLfloat specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	glLightfv(GL_LIGHT0, GL_SPECULAR, specular);
+
+	// Finally, define light source 0 position in World Space
+	GLfloat light_position[] = { 0.0f, 10.0f, 0.0f, 1.0f };
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+}
+
+//=======================================================================
+// Material Configuration Function
+//======================================================================
+void InitMaterial()
+{
+	// Enable Material Tracking
+	glEnable(GL_COLOR_MATERIAL);
+
+	// Sich will be assigneet Material Properties whd by glColor
+	glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+
+	// Set Material's Specular Color
+	// Will be applied to all objects
+	GLfloat specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	glMaterialfv(GL_FRONT, GL_SPECULAR, specular);
+
+	// Set Material's Shine value (0->128)
+	GLfloat shininess[] = { 96.0f };
+	glMaterialfv(GL_FRONT, GL_SHININESS, shininess);
+}
+
+//=======================================================================
+// OpengGL Configuration Function
+//=======================================================================
+void myInit(void)
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+
+	glMatrixMode(GL_PROJECTION);
+
+	glLoadIdentity();
+
+	gluPerspective(fovy, aspectRatio, zNear, zFar);
+	//*******************************************************************************************//
+	// fovy:			Angle between the bottom and top of the projectors, in degrees.			 //
+	// aspectRatio:		Ratio of width to height of the clipping plane.							 //
+	// zNear and zFar:	Specify the front and back clipping planes distances from camera.		 //
+	//*******************************************************************************************//
+
+	glMatrixMode(GL_MODELVIEW);
+
+	glLoadIdentity();
+
+	gluLookAt(Eye.x, Eye.y, Eye.z, At.x, At.y, At.z, Up.x, Up.y, Up.z);
+	//*******************************************************************************************//
+	// EYE (ex, ey, ez): defines the location of the camera.									 //
+	// AT (ax, ay, az):	 denotes the direction where the camera is aiming at.					 //
+	// UP (ux, uy, uz):  denotes the upward orientation of the camera.							 //
+	//*******************************************************************************************//
+
+	InitLightSource();
+
+	InitMaterial();
+
+	glEnable(GL_DEPTH_TEST);
+
+	glEnable(GL_NORMALIZE);
+}
+
+//=======================================================================
+// Render Ground Function
+//=======================================================================
+void RenderGround()
+{
+	glDisable(GL_LIGHTING);	// Disable lighting 
+
+	glColor3f(0.6, 0.6, 0.6);	// Dim the ground texture a bit
+
+	glEnable(GL_TEXTURE_2D);	// Enable 2D texturing
+
+	glBindTexture(GL_TEXTURE_2D, tex_ground.texture[0]);	// Bind the ground texture
+
+	glPushMatrix();
+	glBegin(GL_QUADS);
+	glNormal3f(0, 1, 0);	// Set quad normal direction.
+	glTexCoord2f(0, 0);		// Set tex coordinates ( Using (0,0) -> (5,5) with texture wrapping set to GL_REPEAT to simulate the ground repeated grass texture).
+	glVertex3f(-20, 0, -20);
+	glTexCoord2f(5, 0);
+	glVertex3f(20, 0, -20);
+	glTexCoord2f(5, 5);
+	glVertex3f(20, 0, 20);
+	glTexCoord2f(0, 5);
+	glVertex3f(-20, 0, 20);
+	glEnd();
+	glPopMatrix();
+
+	glEnable(GL_LIGHTING);	// Enable lighting again for other entites coming throung the pipeline.
+
+	glColor3f(1, 1, 1);	// Set material back to white instead of grey used for the ground texture.
+}
+
+//=======================================================================
+// Display Function
+//=======================================================================
+void myDisplay(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+
+	GLfloat lightIntensity[] = { 0.7, 0.7, 0.7, 1.0f };
+	GLfloat lightPosition[] = { 0.0f, 100.0f, 0.0f, 0.0f };
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, lightIntensity);
+
+	// Draw Ground
+	RenderGround();
+
+	// Draw Tree Model
+	//glPushMatrix();
+	//glTranslatef(10, 0, 0);
+	//glScalef(0.7, 0.7, 0.7);
+	//model_tree.Draw();
+	//glPopMatrix();
+
+	//// Draw house Model
+	//glPushMatrix();
+	//glRotatef(90.f, 1, 0, 0);
+	//model_house.Draw();
+	//glPopMatrix();
+
+	// Draw house Model
+	glPushMatrix();
+	glScalef(0.1, 0.1, 0.1);
+
+	//model_home.Draw();
+	glPopMatrix();
+
+	//glPushMatrix();
+	//glScalef(0.2, 0.2, 0.2);
+	//model_water.Draw();
+	//glPopMatrix();
+
+	//glPushMatrix();
+	//glScalef(0.2, 0.2, 0.2);
+	//model_snake.Draw();
+	//glPopMatrix();
+
+	//glPushMatrix();
+	//glScalef(0.2, 0.2, 0.2);
+	//model_rock.Draw();
+	//glPopMatrix();
+
+
+	aladdin.draw();
+
+
+
+
+	//sky box
+	glPushMatrix();
+	GLUquadricObj* qobj;
+	qobj = gluNewQuadric();
+	glTranslated(50, 0, 0);
+	glRotated(90, 1, 0, 1);
+	glBindTexture(GL_TEXTURE_2D, tex);
+	gluQuadricTexture(qobj, true);
+	gluQuadricNormals(qobj, GL_SMOOTH);
+	gluSphere(qobj, 100, 100, 100);
+	gluDeleteQuadric(qobj);
+	glPopMatrix();
+
+	glutSwapBuffers();
+}
+
+//=======================================================================
+// Keyboard Function
+//=======================================================================
+void myKeyboard(unsigned char button, int x, int y)
+{
+	switch (button)
+	{
+	case 'w':
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		break;
+	case 'r':
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		break;
+	case 27:
+		exit(0);
+		break;
+	default:
+		break;
+	}
+
+	glutPostRedisplay();
+}
+
+//=======================================================================
+// Motion Function
+//=======================================================================
+void myMotion(int x, int y)
+{
+	y = HEIGHT - y;
+
+	if (cameraZoom - y > 0)
+	{
+		Eye.x += -0.1;
+		Eye.z += -0.1;
+	}
+	else
+	{
+		Eye.x += 0.1;
+		Eye.z += 0.1;
+	}
+
+	cameraZoom = y;
+
+	glLoadIdentity();	//Clear Model_View Matrix
+
+	gluLookAt(Eye.x, Eye.y, Eye.z, At.x, At.y, At.z, Up.x, Up.y, Up.z);	//Setup Camera with modified paramters
+
+	GLfloat light_position[] = { 0.0f, 10.0f, 0.0f, 1.0f };
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+	glutPostRedisplay();	//Re-draw scene 
+}
+
+//=======================================================================
+// Mouse Function
+//=======================================================================
+void myMouse(int button, int state, int x, int y)
+{
+	y = HEIGHT - y;
+
+	if (state == GLUT_DOWN)
+	{
+		cameraZoom = y;
+	}
+}
+
+//=======================================================================
+// Reshape Function
+//=======================================================================
+void myReshape(int w, int h)
+{
+	if (h == 0) {
+		h = 1;
+	}
+
+	WIDTH = w;
+	HEIGHT = h;
+
+	// set the drawable region of the window
+	glViewport(0, 0, w, h);
+
+	// set up the projection matrix 
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(fovy, (GLdouble)WIDTH / (GLdouble)HEIGHT, zNear, zFar);
+
+	// go back to modelview matrix so we can move the objects about
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(Eye.x, Eye.y, Eye.z, At.x, At.y, At.z, Up.x, Up.y, Up.z);
+}
+
+//=======================================================================
+// Assets Loading Function
+//=======================================================================
+void LoadAssets()
+{
+	// Loading Model filesman
+	aladdin = GameObject({ 0,0,0 }, 0, 0.1, 0.5, "models/aladdin/aladdin.3ds");
+	snake = GameObject({ 0,0,0 }, 0, 0.1, 0.5, "models/aladdin/aladdin.3ds");
+
+
+
+	// model_tree.Load("Models/tree/Tree1.3ds");
+
+	// Loading texture files
+	tex_ground.Load("Textures/ground.bmp");
+	loadBMP(&tex, "Textures/blu-sky-3.bmp", true);
+}
+
+
+
+
+//=======================================================================
+// Main Function
+//=======================================================================
+void main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+
+	glutInitWindowSize(WIDTH, HEIGHT);
+
+	glutInitWindowPosition(100, 150);
+
+	glutCreateWindow(title);
+
+	glutDisplayFunc(myDisplay);
+
+	glutKeyboardFunc(myKeyboard);
+
+	glutMotionFunc(myMotion);
+
+	glutMouseFunc(myMouse);
+
+	glutReshapeFunc(myReshape);
+
+	myInit();
+
+	LoadAssets();
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_NORMALIZE);
+	glEnable(GL_COLOR_MATERIAL);
+
+	glShadeModel(GL_SMOOTH);
+
+
+	glutMainLoop();
+}
